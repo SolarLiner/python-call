476b92b5785f7c2bedf1ee08c90f8868af7538bf
Nathan Graule <solarliner@gmail.com>
2018-08-07 12:32:31 +0200

    Add URL and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -17,11 +17,20 @@ setup(
     name='call',
     version='0.1',
     packages=['call'],
-    url='',
+    url='https://gitlab.com/solarliner/call',
     license='MIT',
     author='Nathan Graule',
     author_email='solarliner@gmail.com',
     description='Thread-based, JS-like asynchronous calls for Python.',
     long_description=readme(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
     test_suite='setup.test_suite'
 )
