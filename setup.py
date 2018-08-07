daa51bc9771acb1f165ee63061bd6ca074a8efa0
Nathan Graule <solarliner@gmail.com>
2018-08-07 10:53:14 +0200

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='call',
+    version='0.1',
+    packages=['call', 'tests'],
+    url='',
+    license='MIT',
+    author='Nathan Graule',
+    author_email='solarliner@gmail.com',
+    description='Thread-based, JS-like asynchronous calls for Python.'
+)
