c88e81fbffe1e43f42ace1cce45d8263bf4a44ec
Nathan Graule <solarliner@gmail.com>
2018-08-07 12:15:24 +0200

    Add readme to long description metadata

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ import unittest
 from setuptools import setup
 
 
+def readme():
+    with open('README.rst', mode='r') as f:
+        return f.read()
+
+
 def test_suite():
     loader = unittest.TestLoader()
     return loader.discover('tests', pattern='test_*.py')
@@ -17,5 +22,6 @@ setup(
     author='Nathan Graule',
     author_email='solarliner@gmail.com',
     description='Thread-based, JS-like asynchronous calls for Python.',
+    long_description=readme(),
     test_suite='setup.test_suite'
 )
